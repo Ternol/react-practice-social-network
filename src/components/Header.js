a4a4edea1c42b8c808678791bfa40edc76c44144
Alex Zavyalov <alexei.tenol@yandex.ru>
2021-07-18 17:54:50 +0500

    changed usual css to css module

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
-import './css/header.css';
+import React from "react";
+import classes from  './css/header.module.css';
 const Header = () => {
     return (
-        <header className="header">
-            <img className="logo" src="https://w7.pngwing.com/pngs/113/271/png-transparent-logo-symbol-sign-company-logo-miscellaneous-leaf-label-thumbnail.png" alt="logo"/>
+        <header className={classes.header}>
+            <img src="https://w7.pngwing.com/pngs/113/271/png-transparent-logo-symbol-sign-company-logo-miscellaneous-leaf-label-thumbnail.png" alt="logo"/>
         </header>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,16 @@
-import './css/nav.css';
+import React from 'react'
+import classes from './css/nav.module.css';
 
 const Nav = () => {
     return (
-        <nav className="nav">
-            <div><a className="item">Профиль</a></div>
-            <div><a className="item">Сообщения</a></div>
-            <div><a className="item">Новости</a></div>
-            <div><a className="item">Музыка</a></div>
-            <div><a className="item">Настройки</a></div>
+        <nav className={classes.nav}>
+            <div><a className={classes.item} href="#">Профиль</a></div>
+            <div><a className={classes.item} href="#">Сообщения</a></div>
+            <div><a className={classes.item} href="#">Новости</a></div>
+            <div><a className={classes.item} href="#">Музыка</a></div>
+            <div><a className={classes.item} href="#">Настройки</a></div>
         </nav>
     )
 }
 
+export default Nav;
